7d062ec7ae760c867b642c38de2429ce9c34ffc5
Adrian Avend <adrian.avend@gmail.com>
2020-08-09 22:48:17 -0300

    Input, Output y viewchild

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input } from '@angular/core';
+
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label, Color } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styles: [
+  ]
+})
+export class DonaComponent {
+
+  @Input() title: string = 'Sin titulo';
+  // Doughnut1
+  @Input('labels') doughnutChartLabels: Label[] = ['Label 1', 'Label 2', 'Label 3'];
+  @Input('data') doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    { backgroundColor: ['#6857E6', '#009FEE', '#F02059' ]}
+  ];
+
+  constructor() { }
+  
+  // events
+  // public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  //   console.log(event, active);
+  // }
+
+  // public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  //   console.log(event, active);
+  // }
+
+
+}
